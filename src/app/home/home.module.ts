9b345de8a985e3d45b1dd3391f431b4af71b1a5c
Winn Nie <winnnie.wong@hexainitiative.com>
2022-05-06 18:01:18 +0800

    [] ngx-extended-pdf-viewer

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 
 
 @NgModule({
@@ -12,7 +13,8 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    NgxExtendedPdfViewerModule
   ],
   declarations: [HomePage]
 })
